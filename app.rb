3bc625bc78dc9bd09d748391ffd1ac46f86893f0
congo-quinine <congo.quinine@gmail.com>
2018-10-29 15:45:32 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Lord Gojira"
+  end
+
+  get '/hometown' do
+      "My hometown is Japan"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Crash and Burn"
+  end
+
 end
